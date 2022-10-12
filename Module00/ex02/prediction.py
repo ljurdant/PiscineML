ef71f5bec05239151f12a97a5c5111ce2a36bff7
Louis JURDANT <ljurdant@studebt.42.fr>
2022-10-12 19:15:54 +0200

    ex02

---
+++ Module00/ex02/prediction.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def simple_predict(x, theta):
+    """Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
+    Args:
+        x: has to be an numpy.ndarray, a vector of dimension m * 1.
+        theta: has to be an numpy.ndarray, a vector of dimension 2 * 1.
+    Returns:
+        y_hat as a numpy.ndarray, a vector of dimension m * 1.
+        None if x or theta are empty numpy.ndarray.
+        None if x or theta dimensions are not appropriate.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or not isinstance(theta, np.ndarray):
+        return None
+    if len(x) == 0 or not len(x.shape) == 1 or not theta.shape == (2,):
+        return None
+
+    return (theta[1]*x+theta[0])
---
+++ Module00/ex02/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from prediction import simple_predict
+x = np.arange(1,6)
+# Example 1:
+theta1 = np.array([5, 0])
+print(simple_predict(x, theta1))
+theta2 = np.array([0, 1])
+print(simple_predict(x, theta2))
+theta3 = np.array([5, 3])
+print(simple_predict(x, theta3))
+theta4 = np.array([-3, 1])
+print(simple_predict(x, theta4))
+
+
+print(simple_predict(np.array([]), theta4))
+print(simple_predict(x, np.array([])))
+print(simple_predict(x, 3))
