f0d66d012ab74a48f3484fa2f2c3ea8f15ecfa3f
Louis JURDANT <ljurdant@studebt.42.fr>
2022-12-03 17:48:44 +0100

    ex04;

--- Module01/ex04/linear_model.py
+++ Module01/ex04/linear_model.py
@@ -3,14 +3,19 @@ import sys, os, numpy as np, pandas as pd, matplotlib.pyplot as plt
 dirname = os.path.dirname(os.path.abspath(__file__))[:-4]+"ex03"
 sys.path.append(dirname)
 
+
 from my_linear_regression import MyLinearRegression as MyLR
 
-data = pd.read_csv("are_blue_pills_magic.csv")
-Xpill = np.array(data[Micrograms]).reshape(-1,1)
-Yscore = np.array(data[Score]).reshape(-1,1)
+data = pd.read_csv("../data/are_blue_pills_magic.csv")
+Xpill = np.array(data["Micrograms"]).reshape(-1,1)
+Yscore = np.array(data["Score"]).reshape(-1,1)
 
 linear_model1 = MyLR(np.array([[89.0], [-8]]))
 linear_model1.fit_(Xpill, Yscore)
 Y_model1 = linear_model1.predict_(Xpill)
-plt.scatter(X_pill, Yscore)
+plt.plot(Xpill, Yscore, 'o')
+plt.plot(Xpill, Y_model1, linestyle="dashed", marker='s')
+plt.legend(["Spredict(pills)","Strue(pills)"],bbox_to_anchor=(0,1.1), loc="upper left", ncol=2, edgecolor="white")
+plt.xlabel("Quantity of blue pills (in micrograms)")
+plt.ylabel("Space driving score")
 plt.show()
